e8f8c3e77141d74bc1ba630ea666f0ea80a8955f
kacper-cf <kacper@couponfollow.com>
2022-11-24 20:24:02 +0100

    filter out empty fields in delivery details

--- letters/letter.tsx
+++ letters/letter.tsx
@@ -32,7 +32,15 @@ export const renderLetter = (responseObject: WishlistModel): JSX.Element => {
               ))}
           </Tbody>
         </Table>
-        <p style={{ width: "80%", margin: "40px auto", textAlign: "center", fontWeight:'bold', fontSize:'1.5rem' }}>
+        <p
+          style={{
+            width: "80%",
+            margin: "40px auto",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "1.5rem",
+          }}
+        >
           Aby osoba mogła otrzymać Twój prezent wyślij go na adres, który podała
           w formularzu,
           <br /> pamiętaj jednak, żeby wysłać prezent maksymalnie do 9 grudnia
@@ -53,7 +61,14 @@ export const renderLetter = (responseObject: WishlistModel): JSX.Element => {
           </tbody>
         </Table>
 
-        <p style={{ width: "80%", margin: "0 auto", textAlign: "justify",fontSize:'1.2rem' }}>
+        <p
+          style={{
+            width: "80%",
+            margin: "0 auto",
+            textAlign: "justify",
+            fontSize: "1.2rem",
+          }}
+        >
           Po wysłaniu prezentu pamiętaj, żeby wysłać nam w odpowiedzi do tej
           wiadomości numer do śledzenia paczki, co da nam możliwość sprawdzenia
           czy obdarowywany otrzymał swój prezent. Skoro otrzymałeś tą wiadomość
@@ -65,9 +80,16 @@ export const renderLetter = (responseObject: WishlistModel): JSX.Element => {
           prywatnego na publiczny, żeby Mikołaj mógł na podstawie twoich postów
           dowiedzieć się o Tobie trochę więcej. Jeśli wszystko jest jasne, to
           odpowiedz na tę wiadomość. Daj nam znać, że do Ciebie dotarła.
-          (wystarczy, że klikniesz serduszko pod tą wiadomością) Z
-          poważaniem,Elfowi pomocnicy! P.S. Jeśli masz jakiś problem lub pytania
-          w pierwszej kolejności napisz do nas.
+          (wystarczy, że klikniesz serduszko pod tą wiadomością) 
+          <br />
+          <br/>
+          Z poważaniem,
+          Elfowi pomocnicy! <br />
+          <br/>
+          <b>
+            P.S. Jeśli masz jakiś problem lub pytania w pierwszej kolejności
+            napisz do nas.
+          </b>
         </p>
       </Flex>
     </Container>
